fb6947d03e51ec4e3027402d457881c08d81fc5a
ARJUN <arjunsharman06@gmail.com>
2022-06-08 11:42:03 -0400

    Added Query String

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -34,8 +34,9 @@ var displayRepos = function (repos, searchTerm) {
     var repoName = repos[i].owner.login + "/" + repos[i].name;
 
     // create a container for each repo
-    var repoEl = document.createElement("div");
+    var repoEl = document.createElement("a");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
     // create a span element to hold repository name
     var titleEl = document.createElement("span");
@@ -67,7 +68,7 @@ var displayRepos = function (repos, searchTerm) {
 var getUserRepos = function (user) {
 
   // format the github api url
-  var apiUrl = "https://api.github1adsdadad3edwd.com/users/" + user + "/repos";
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
   // make a request to the url
   fetch(apiUrl)
